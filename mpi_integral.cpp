7e9c14b4581b7a1ebe5837b2425f467e54656fba
shyringo <shyringo@163.com>
2020-09-24 13:26:29 +0000

    9-24night

--- mpi_integral.cpp
+++ mpi_integral.cpp
@@ -6,13 +6,13 @@ int main(int argc,char** argv)
 {
     int myid,numprocs;
     MPI_Status status;
-    double local=0,dx-double(10/N),integral;
+    double local=0,dx=double(10)/double(N),integral=0;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     if(myid!=0){
         for(int i=myid-1;i<N;i=i+numprocs-1){
-            x=i*dx+dx/2;
+            double x=i*dx+dx/2;
             local+=x*x*dx;
         }
         MPI_Send(&local,1,MPI_DOUBLE,0,1,MPI_COMM_WORLD);
@@ -23,7 +23,7 @@ int main(int argc,char** argv)
             MPI_Recv(&d,1,MPI_DOUBLE,i,1,MPI_COMM_WORLD,&status);
             integral+=d;
         }
-        cout<<"the integral of x*x in [0,10] is "<<integral;
+        cout<<"the integral of x*x in [0,10] is "<<integral<<endl;
     }
     MPI_Finalize();
 }
--- mpi_sqrtSum.cpp
+++ mpi_sqrtSum.cpp
@@ -5,18 +5,27 @@ using namespace std;
 int main(int argc,char** argv)
 {
     int myid,numprocs,N;
-    cin>>N;//计算0到N-1的开方求和
-    double SqrtSum=0,local=0;
+    double SqrtSum,local=0;
     MPI_Status status;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    for(int i=myid;i<=N-1;i=i+numprocs){
+    if(myid==0){
+	cout<<"plz enter N:";
+	cin>>N;
+	for(int j=1;j<numprocs;j++){
+	MPI_Send(&N,1,MPI_INT,j,1,MPI_COMM_WORLD);
+	}
+    }
+    else{
+        MPI_Recv(&N,1,MPI_INT,0,1,MPI_COMM_WORLD,&status);
+    }
+    for(int i=myid;i<=N-1;i+=numprocs){
         local+=sqrt(i);
     }
     MPI_Reduce(&local,&SqrtSum,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
     if(myid==0){
-        cout<<"the sum of the respective sqrt of 0 to "<<N-1<<" is SqrtSum";
+        cout<<"the sum of the respective sqrt of 0 to "<<N-1<<" is "<<SqrtSum<<endl;
     }
     MPI_Finalize();
 }
